910bd7e3af3ce0b23c111d9c1e12aaeaab1fc01b
MohamadMF <72653078+MohamadMF@users.noreply.github.com>
2025-02-23 10:04:30 +0200

    Push what moved from AndroidApp Rep

---
+++ Level02AndroidApps/PhotoPicker/app/src/main/java/com/example/photopicker/ListViewLayout.kt
@@ -0,0 +1,29 @@
+package com.example.photopicker
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListViewLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_view_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val animals = arrayOf("Lion", "Tiger", "Elephant", "Zipper", "Cat","Lion", "Tiger", "Elephant", "Zipper", "Cat","Lion", "Tiger", "Elephant", "Zipper", "Cat")
+        val animalAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,animals)
+        val animalsViewLayout: ListView = findViewById(R.id.animals)
+        animalsViewLayout.adapter = animalAdapter
+
+
+    }
+}
---
+++ Level02AndroidApps/PhotoPicker/app/src/main/java/com/example/photopicker/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.photopicker
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.photopicker.ui.theme.PhotoPickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PhotoPickerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        setContentView(R.layout.activity_main)
+        val clickedBtn: Button = findViewById(R.id.clicked_btn)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PhotoPickerTheme {
+        Greeting("Android")
+    }
+}
