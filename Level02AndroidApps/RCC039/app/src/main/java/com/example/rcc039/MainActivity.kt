2c50246caa0c5a36674fc9e9f191b77c58033ee4
MohamadMF <72653078+MohamadMF@users.noreply.github.com>
2025-02-28 01:04:59 +0200

    complete MainActivity.kt and .xml

--- Level02AndroidApps/RCC039/app/src/main/java/com/example/rcc039/MainActivity.kt
+++ Level02AndroidApps/RCC039/app/src/main/java/com/example/rcc039/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.rcc039
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,63 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle","onCreate() has called")
+
+//        val emailInput: EditText = findViewById(R.id.email_text_id)
+        val nameInput: EditText = findViewById(R.id.name_text_id)
+        val loginBtn: Button = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener { //TODO("Text Watcher")
+            val writtenText = nameInput.text.toString()
+            if (writtenText.isBlank()) {//if it was isEmpty, it won't ignore any spaces
+                Toast.makeText(this,
+                    "You didn't enter a name yet!",
+                    Toast.LENGTH_SHORT)
+                    .show()
+                nameInput.error = "Required field"
+            }
+            else
+                Toast.makeText(this,
+                    "Hello $writtenText!",
+                    Toast.LENGTH_SHORT)
+                    .show()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("lifecycle","onStart() has called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("lifecycle","onRestart() has called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifecycle","onResume() has called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // for printing anything in app, we determine (Device - Category - Subcategory)
+
+        // when I wanna display a message, I need two things:
+        //   1. address --> key in Log.d() and that will go to receiver
+        //   2. letter --> value in Log.d()
+        Log.d("lifecycle","onPause() has called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lifecycle","onStop() has called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lifecycle","onDestroy() has called")
     }
 }
